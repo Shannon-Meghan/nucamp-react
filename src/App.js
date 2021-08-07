e223744079bdf1c7f393eb7112d747db28f8c6a5
Meghan Shannon <shannon.mss@gmail.com>
2021-08-06 22:43:56 -0400

    Controlled Forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Main />
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Main />
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
-import 'typeface-lobster';
-import 'typeface-open-sans';
-import '../App.css';
+import {
+	Nav,
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	Collapse,
+	NavItem,
+	Jumbotron,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+	constructor(props) {
+		super(props);
+
+		this.toggleNav = this.toggleNav.bind(this);
+		this.state = {
+			isNavOpen: false,
+		};
+	}
+
+	toggleNav() {
+		this.setState({
+			isNavOpen: !this.state.isNavOpen,
+		});
+	}
+
 	render() {
 		return (
 			<React.Fragment>
@@ -18,9 +39,41 @@ class Header extends Component {
 						</div>
 					</div>
 				</Jumbotron>
-				<Navbar dark sticky="top">
+				<Navbar dark sticky="top" expand="md">
 					<div className="container">
-						<NavbarBrand href="/">NuCamp</NavbarBrand>
+						<NavbarBrand className="mr-auto" href="/">
+							<img
+								src="/assets/images/logo.png"
+								height="30"
+								width="30"
+								alt="NuCamp Logo"
+							/>{' '}
+						</NavbarBrand>
+						<NavbarToggler onClick={this.toggleNav} />
+						<Collapse isOpen={this.state.isNavOpen} navbar>
+							<Nav navbar>
+								<NavItem>
+									<Link class="nav-link" to="/home">
+										<i className="fa fa-home fa-lg" /> Home
+									</Link>
+								</NavItem>
+								<NavItem>
+									<Link class="nav-link" to="/directory">
+										<i className="fa fa-list fa-lg" /> Directory
+									</Link>
+								</NavItem>
+								<NavItem>
+									<Link class="nav-link" to="/aboutus">
+										<i className="fa fa-info fa-lg" /> About
+									</Link>
+								</NavItem>
+								<NavItem>
+									<Link class="nav-link" to="/contactus">
+										<i className="fa fa-address-card fa-lg" /> Contact Us
+									</Link>
+								</NavItem>
+							</Nav>
+						</Collapse>
 					</div>
 				</Navbar>
 			</React.Fragment>
